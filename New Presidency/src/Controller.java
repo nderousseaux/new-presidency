25e7ad15beb854cccb291a094a2a4eee67724925
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-09-30 16:42:44 +0200

    Insertion du début de l'interface

--- New Presidency/src/Controller.java
+++ New Presidency/src/Controller.java
@@ -2,12 +2,6 @@ public class Controller {
     G_Indicator g_indicator;
     G_Lever g_lever;
 
-    public static void main(String[] args){
-        Indicator.initIndicators();
-        //Lever.initLevers();
-
-    }
-
     public void tour(){
         //tour de jeu...
 
