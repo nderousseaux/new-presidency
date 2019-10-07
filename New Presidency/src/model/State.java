5ee15cdecfc185bbc05dd53b7eba366e360b0320
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-10-07 23:04:30 +0200

    Corrections syntaxiques et annotations

--- New Presidency/src/model/State.java
+++ New Presidency/src/model/State.java
@@ -3,19 +3,19 @@ package model;
 public class State{
     //attributs (i devant pour indicateurs et l devant pour leviers)
     private List_States historical;
-    private int _year; // ni un indicateur ni un levier
-    private float _iBudget;
+    private int _year; // ni un indicateur ni un levier YOAN: Remplacer les années par des semestres?
+    private float _iBudget; // YOAN : Le budget de quoi? Total? Du coup pas un indicateur?
     private float _lRegistrationFees;
     private int _iStudentNumber;
     private float _iStudentSatisfaction;
     
     //constructeur
     public State(int year, float iBudget, float lRegistrationFees, int iStudentNumber, float iStudentSatisfaction){
-        this._year = year;
-        this._iBudget = iBudget;
-        this._lRegistrationFees = lRegistrationFees;
-        this._iStudentNumber = iStudentNumber;
-        this._iStudentSatisfaction = iStudentSatisfaction;
+        _year = year;
+        _iBudget = iBudget;
+        _lRegistrationFees = lRegistrationFees;
+        _iStudentNumber = iStudentNumber;
+        _iStudentSatisfaction = iStudentSatisfaction;
     }
     
     //accesseur
@@ -43,33 +43,32 @@ public class State{
     //seteur
 
     public void setYear(int year) {
-        this._year = year;
+        _year = year;
     }
 
     public void setIBudget(float iBudget) {
-        this._iBudget = iBudget;
+        _iBudget = iBudget;
     }
 
     public void setLRegistrationFees(float lRegistrationFees) {
-        this._lRegistrationFees = lRegistrationFees;
+        _lRegistrationFees = lRegistrationFees;
     }
 
     public void setIStudentNumber(int iStudentNumber) {
-        this._iStudentNumber = iStudentNumber;
+        _iStudentNumber = iStudentNumber;
     }
 
-    public void setIStudentSatisfaction(float _iStudentSatisfaction) {
-        this._iStudentSatisfaction = _iStudentSatisfaction;
+    public void setIStudentSatisfaction(float _iStudentSatisfaction) {_iStudentSatisfaction = _iStudentSatisfaction;
     }
 
     //méthodes
     //passage vers l'état suivant (c'est à dire vers l'année suivante)
     public State generateNextState(){
         int nextYear = this._year + 1;
-        float nextRegistrationFees = this._lRegistrationFees;
-        float nextStudentSatisfaction = this.generateNextStudentSatisfaction();
-        int nextStudentNumber = this.generateNextStudentNumber(nextStudentSatisfaction);
-        float nextBudget = this._iBudget + (nextStudentNumber * nextRegistrationFees);
+        float nextRegistrationFees = _lRegistrationFees;
+        float nextStudentSatisfaction = generateNextStudentSatisfaction();
+        int nextStudentNumber = generateNextStudentNumber(nextStudentSatisfaction);
+        float nextBudget = _iBudget + (nextStudentNumber * nextRegistrationFees);
         //voir si on ajoute une somme qui est reçue chaque année du gouvernement
         
         State nextState = new State(nextYear,nextBudget,nextRegistrationFees,nextStudentNumber,nextStudentSatisfaction);
@@ -78,18 +77,18 @@ public class State{
     
     //calcul de certains indicateurs
     public float generateNextStudentSatisfaction(){
-        float nextStudentSatisfaction = this._iStudentSatisfaction;
-        State previousState = historical.getState(this._year - 1);
+        float nextStudentSatisfaction = _iStudentSatisfaction;
+        State previousState = historical.getState(_year - 1);
         float previousRegistrationFees = previousState.getLRegistrationFees();
         //si les frais d'inscription ont augmentés
-        if(this._lRegistrationFees > previousRegistrationFees){
-            nextStudentSatisfaction = this._iStudentSatisfaction * (0 - (this._lRegistrationFees/previousRegistrationFees));
+        if(_lRegistrationFees > previousRegistrationFees){
+            nextStudentSatisfaction = _iStudentSatisfaction * (0 - (_lRegistrationFees/previousRegistrationFees));
             // dans ce cas la satisfaction baisse proportionellement à l'augmentation des frais d'inscriptions
             //on peut procéder autrement, à discuter
         }
         //si les frais d'inscriptions ont diminués
-        if(this._lRegistrationFees < previousRegistrationFees){
-            nextStudentSatisfaction = this._iStudentSatisfaction * (previousRegistrationFees/this._lRegistrationFees);
+        if(_lRegistrationFees < previousRegistrationFees){
+            nextStudentSatisfaction = _iStudentSatisfaction * (previousRegistrationFees/_lRegistrationFees);
             // dans ce cas la satisfaction augmente proportionellement à la diminution des frais d'inscriptions
             //on peut procéder autrement, à discuter
         }
@@ -97,17 +96,17 @@ public class State{
     }
     
     public int generateNextStudentNumber(float nextStudentSatisfaction){
-        int nextStudentNumber = this._iStudentNumber;
+        int nextStudentNumber = _iStudentNumber;
         // formule pas du tout valide : exemple dan ce cas si on a 100 étudiants à l'an 0
         //et que la satisfaction est de 80% , à l'an 1 on aura 180 étudiants
         //deuxièmement , si la satisfaction est de 40% , on aura 60 étudiants à l'an 1
         //mais si la satisfaction est de 1% on aura 99 étudiants à l'an 1
         // BREF j'ai juste mis des formules pour l'exemple
         if(nextStudentSatisfaction > 0.5){
-            nextStudentNumber += nextStudentSatisfaction * this._iStudentNumber;
+            nextStudentNumber += nextStudentSatisfaction * _iStudentNumber;
         }
         if(nextStudentSatisfaction < 0.5){
-            nextStudentNumber -= nextStudentSatisfaction * this._iStudentNumber;
+            nextStudentNumber -= nextStudentSatisfaction * _iStudentNumber;
         }
         return nextStudentNumber;
     }
