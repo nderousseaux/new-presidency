4540b466ffa5e8b828bdfdfb8bb5538b9fcf7b20
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-10-21 16:33:01 +0200

    Conception de l'interface graphique

--- New Presidency/src/view/GraphicalView.java
+++ New Presidency/src/view/GraphicalView.java
@@ -16,24 +16,35 @@ public class GraphicalView extends JFrame {
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(1200,850);
-        this.setVisible(true);
+
         this.setTitle("New Presidency");
         this.setLayout(new BorderLayout());
 
         JPanel year = new JPanel();
-        this.getContentPane().add(year,BorderLayout.NORTH);
-        year.add(new JTextArea("Année "+_controller.getYear()+" sur "+_controller.getMaxYear()));
-        this.add(year);
 
-        JScrollPane levers=new JScrollPane(new TextArea("Levers"));
-        this.getContentPane().add(levers,BorderLayout.WEST);
-        JTextArea leverList = new JTextArea(_controller.getLevers().size(),1);
+        year.add(new JTextArea("Année "+_controller.getYear()+" sur "+_controller.getMaxYear()));
+        this.getContentPane().add(year,BorderLayout.NORTH);
+
+
+        JPanel leverpan=new JPanel();
+
+
+
+        leverpan.setLayout(new GridLayout(_controller.getLevers().size(),100));
+        this.getContentPane().add(leverpan,BorderLayout.WEST);
         int i=1;
-        for(Lever l : _controller.getLevers()){
-            levers.add(new Label(l.getName()));
+        for(Lever l : _controller.getLevers()) {
+            leverpan.add(new JTextArea(l.getName()),i);
             i++;
         }
-        this.add(levers);
+        JScrollPane scroll=new JScrollPane(leverpan,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.setBounds(20,40,500,610);
+        this.getContentPane().add(scroll,BorderLayout.WEST);
+        this.getContentPane().add(leverpan, BorderLayout.WEST);
 
+
+        this.add(year);
+
+        this.setVisible(true);
     }
 }
