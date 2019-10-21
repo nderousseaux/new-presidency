6610a9607b2499407468dce65534b09beeb62a13
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-10-21 14:55:27 +0200

    Refactor des noms de vues

--- New Presidency/src/Main.java
+++ New Presidency/src/Main.java
@@ -1,14 +1,10 @@
 import controller.Controller;
-import model.Indicator;
-import model.Lever;
-import view.Textual_Interface;
-
-import java.util.Scanner;
+import view.TextualView;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException{
         Controller controller=new Controller();
-        Textual_Interface affichage = new Textual_Interface(controller);
+        TextualView affichage = new TextualView(controller);
         controller.init();
         affichage.init();
     }
