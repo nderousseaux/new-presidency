a0bf5dcdd427a4e11348e249f304a0032ad0ce0b
TREYER LUCAS <lucas.treyer@etu.unistra.fr>
2019-10-31 18:13:48 +0100

    correction des problèmes d'encodages

--- New Presidency/src/controller/Controller.java
+++ New Presidency/src/controller/Controller.java
@@ -41,8 +41,8 @@ public class Controller {
         //Lever recTitulaire =  _leverList.createLever("Titulaires en recherche", 100, new ArrayList<String>());
         //Lever recContractuel = _leverList.createLever("Contractuels en recherche", 100, new ArrayList<String>());
         ArrayList<String> infosRecDotRec=new ArrayList<>();
-        infosRecDotRec.add("Budget rÃ©current allouÃ© ï¿½ la recherche");
-        Lever recDotRec = _leverList.createLever("Dotation rÃ©curante en recherche", 100.0,10000.0, infosRecDotRec);
+        infosRecDotRec.add("Budget récurrent alloué à la recherche");
+        Lever recDotRec = _leverList.createLever("Dotation récurante en recherche", 100.0,10000.0, infosRecDotRec);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-recDotRec.getBudget());
         //Lever recDotSpe = _leverList.createLever("Dotation spÃ©cifique en recherche", 100, new ArrayList<String>());
         //Lever recValorisation = _leverList.createLever("Valorisation de la recherche", 100, new ArrayList<String>());
@@ -54,37 +54,37 @@ public class Controller {
         //Lever formTitulaire = _leverList.createLever("Titulaires en formation", 100, new ArrayList<String>());
         //Lever formContractuel = _leverList.createLever("Contractuels en formation", 100, new ArrayList<String>());
         ArrayList<String> infosFormDotRec=new ArrayList<>();
-        infosFormDotRec.add("Budget rÃ©current allouÃ© aux formations");
-        Lever formDotRec = _leverList.createLever("Dotation rÃ©curente pour la formation", 100.0,10000.0, infosFormDotRec);
+        infosFormDotRec.add("Budget récurrent alloué aux formations");
+        Lever formDotRec = _leverList.createLever("Dotation récurente pour la formation", 100.0,10000.0, infosFormDotRec);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-formDotRec.getBudget());
 
         ArrayList<String> infosFormDotSpe= new ArrayList<>();
-        infosFormDotSpe.add("Budget spÃ©cifique allouÃ© aux formations");
-        Lever formDotSpe = _leverList.createLever("Dotation spÃ©cifique pour la formation", 100.0,10000.0, infosFormDotSpe);
+        infosFormDotSpe.add("Budget spécifique alloué aux formations");
+        Lever formDotSpe = _leverList.createLever("Dotation spécifique pour la formation", 100.0,10000.0, infosFormDotSpe);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-formDotSpe.getBudget());
         //Lever formDotPed = _leverList.createLever("Dotation pour les projets pÃ©dagogiques", 100, new ArrayList<String>());
         //Lever formPartenariat = _leverList.createLever("Partenariat pour la formation", 100, new ArrayList<String>());
         //Lever formFraiIns = _leverList.createLever("Frais d'inscription", 100, new ArrayList<String>());
 
         ArrayList<String> infosFormPrime= new ArrayList<>();
-        infosFormPrime.add("Budget allouÃ© Ã  la prime de formation des enseignants");
-        infosFormPrime.add("Des enseignants plus formÃ©s donnent de meilleurs cours!");
+        infosFormPrime.add("Budget alloué à  la prime de formation des enseignants");
+        infosFormPrime.add("Des enseignants plus formés donnent de meilleurs cours!");
         Lever formPrime= _leverList.createLever("Prime de formation", 100.0,10000.0, infosFormPrime);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-formPrime.getBudget());
         //Central
         //Lever cCom = _leverList.createLever("Communication gÃ©nÃ©rale", 100, new ArrayList<String>());
 
         ArrayList<String> infosSubEtu=new ArrayList<>();
-        infosSubEtu.add("Budget allouÃ© aux associations Ã©tudiantes");
-        infosSubEtu.add("L'augmenter augmentera l'humeur des Ã©lÃ¨ves dans l'universitÃ©, mais attention!");
-        infosSubEtu.add("Buvez toujours avec ModÃ©ration (trÃ¨s bon ami des programmeurs)!");
-        Lever subEtu = _leverList.createLever("Subventions aux associations Ã©tudiantes", 100.0,10000.0, infosSubEtu);
+        infosSubEtu.add("Budget alloué aux associations étudiantes");
+        infosSubEtu.add("L'augmenter augmentera l'humeur des étudiants dans l'université, mais attention!");
+        infosSubEtu.add("Buvez toujours avec Modération (très bon ami des programmeurs)!");
+        Lever subEtu = _leverList.createLever("Subventions aux associations étudiantes", 100.0,10000.0, infosSubEtu);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-subEtu.getBudget());
         //Immobilier
 
         ArrayList<String> infosCons=new ArrayList<>();
-        infosCons.add("Budget gÃ©nÃ©ral de l'immobilier");
-        infosCons.add("De meilleurs bÃ¢timents apportent un niveau d'enseignement amÃ©liorÃ©");
+        infosCons.add("Budget général de l'immobilier");
+        infosCons.add("De meilleurs batiments apportent un niveau d'enseignement amélioré");
         Lever constr = _leverList.createLever("Investissement en construction", 100.0,10000.0, infosCons);
         _budget.setRemainingBudget(_budget.getRemainingBudget()-constr.getBudget());
         //Lever iEnt = _leverList.createLever("Investissement en entretient des bÃ¢timents", 100, new ArrayList<String>());
@@ -128,15 +128,15 @@ public class Controller {
 
         //Taux de rÃ©ussite
         ArrayList<String> infosReu=new ArrayList<>();
-        infosReu.add("Niveau de rÃ©ussite de la formation");
-        infosReu.add("ReprÃ©sente par extension le niveau d'enseignement de la formation et l'encadrement des Ã©lÃ¨ves");
+        infosReu.add("Niveau de réussite de la formation");
+        infosReu.add("Représente par extension le niveau d'enseignement de la formation et l'encadrement des étudiants");
         Indicator tauxReussite = _indicatorList.createIndicator("Taux de réussite du diplôme", 50,100,infosReu);
 
         //Satisfaction etudiante
 
         ArrayList<String> infosSatisEtu=new ArrayList<>();
-        infosSatisEtu.add("ReprÃ©sente le niveau d'apprÃ©ciations des Ã©tudiants");
-        infosSatisEtu.add("Attention Ã  ne pas oublier qu'un Ã©lÃ¨ve qui s'amuse trop travaille moins...");
+        infosSatisEtu.add("Représente le niveau d'appréciations des étudiants");
+        infosSatisEtu.add("Attention à  ne pas oublier qu'un étudiant qui s'amuse trop travaille moins...");
         Indicator satisEtu = _indicatorList.createIndicator("Satisfaction etudiante", 50,100, infosSatisEtu);
 
 
@@ -144,8 +144,8 @@ public class Controller {
         //Satisfaction personnel
 
         ArrayList<String> infosSatisPers=new ArrayList<>();
-        infosSatisPers.add("ReprÃ©sente le niveau d'apprÃ©ciation des professeur");
-        infosSatisPers.add("Attention Ã  ne pas oublier qu'un professeur qui ne se plait pas dans son travail enseigne moins bien...");
+        infosSatisPers.add("Représente le niveau d'appréciation des professeur");
+        infosSatisPers.add("Attention à  ne pas oublier qu'un professeur qui ne se plait pas dans son travail enseigne moins bien...");
 
 
         Indicator satisPers = _indicatorList.createIndicator("Satisfaction personnel",50,100,infosSatisPers);
@@ -158,7 +158,7 @@ public class Controller {
 
     }
 
-    
+
     public Integer addToBudget(Lever lever, Double val){
         Integer r;
         if(val<=_budget.getRemainingBudget()) {
@@ -189,27 +189,27 @@ public class Controller {
         //tour de jeu...
         State thisYearState = _stateList.getState(_year);
         for(Lever l : _leverList.getLevers()){
-            
-            if(l.getName() == "Dotation rÃ©curante en recherche"){
+
+            if(l.getName() == "Dotation récurante en recherche"){
                 thisYearState.setLDotRecRech(l.getBudget());
             }
-            
-            if(l.getName() == "Dotation rÃ©curente pour la formation"){
+
+            if(l.getName() == "Dotation récurente pour la formation"){
                 thisYearState.setLDotRecForm(l.getBudget());
             }
-            
-            if(l.getName() == "Dotation spÃ©cifique pour la formation"){
+
+            if(l.getName() == "Dotation spécifique pour la formation"){
                 thisYearState.setLDotSpeForm(l.getBudget());
             }
-            
+
             if(l.getName() == "Prime de formation"){
                 thisYearState.setLPrime(l.getBudget());
             }
-            
-            if(l.getName() == "Subventions aux associations Ã©tudiantes"){
+
+            if(l.getName() == "Subventions aux associations étudiantes"){
                 thisYearState.setLSubAssoEtu(l.getBudget());
             }
-            
+
             if(l.getName() == "Investissement en construction"){
                 thisYearState.setLImmo(l.getBudget());
             }
@@ -249,57 +249,57 @@ public class Controller {
     public void updateAll(){
         State thisYearState = _stateList.getState(_year);
         State lastYearState = _stateList.getState((_year - 1));
-        
+
         double resSatEtu = lastYearState.getISatEtu();
         double resSatPers = lastYearState.getISatPers();
         double resTauxReu = lastYearState.getITauxReu();
-        
+
         double dLSubAssoEtu = (thisYearState.getLSubAssoEtu() - lastYearState.getLSubAssoEtu()) / lastYearState.getLSubAssoEtu();
         double dLDotRecForm = (thisYearState.getLDotRecForm() - lastYearState.getLDotRecForm()) / lastYearState.getLDotRecForm();
         double dLDotRecRech = (thisYearState.getLDotRecRech() - lastYearState.getLDotRecRech()) / lastYearState.getLDotRecRech();
         double dLDotSpeForm = (thisYearState.getLDotSpeForm() - lastYearState.getLDotSpeForm()) / lastYearState.getLDotSpeForm();
         double dLImmo = (thisYearState.getLImmo() - lastYearState.getLImmo()) / lastYearState.getLImmo();
         double dLPrime = (thisYearState.getLPrime() - lastYearState.getLPrime()) / lastYearState.getLPrime();
-        
+
         //Pour chaque indicateur on calcule la valeur au tour suivant
-        
+
         //calcul de la satisfaction etudiante
         Indicator i = _indicatorList.getIndicator("Satisfaction etudiante");
-        
+
         resSatEtu += dLSubAssoEtu*(1.0/3.0)*(lastYearState.getISatEtu()/i.getMaxValue());
-                
+
         resSatEtu += dLImmo * (1.0/3.0) * (lastYearState.getISatEtu()/i.getMaxValue());
-            
-        i.setValue(resSatEtu); 
-        
-        
-        // calcul de la satisfaction du personel 
+
+        i.setValue(resSatEtu);
+
+
+        // calcul de la satisfaction du personel
         i = _indicatorList.getIndicator("Satisfaction personnel");
-        
+
         resSatPers += dLPrime*(1.0/5.0)*(lastYearState.getISatPers()/i.getMaxValue());
-              
-        resSatPers += dLDotRecForm*(1.0/5.0)*(lastYearState.getISatPers()/i.getMaxValue());               
+
+        resSatPers += dLDotRecForm*(1.0/5.0)*(lastYearState.getISatPers()/i.getMaxValue());
 
         resSatPers += dLDotRecRech*(1.0/5.0)*(lastYearState.getISatPers()/i.getMaxValue());
 
         resSatPers += dLImmo * (1.0/5.0) * (lastYearState.getISatPers()/i.getMaxValue());
-            
+
         i.setValue(resSatPers);
-        
-        //creation de la variable contenant la différence entre la satisfaction du personel de l'année dernière et de cette année    
+
+        //creation de la variable contenant la différence entre la satisfaction du personel de l'année dernière et de cette année
         double dISatPers = (resSatPers - lastYearState.getISatPers()) / lastYearState.getISatPers();
-        
+
         //calcul du taux de reussite du diplome
-        i = _indicatorList.getIndicator("Taux de réussite du diplôme");   
+        i = _indicatorList.getIndicator("Taux de réussite du diplôme");
 
         resTauxReu += dLDotSpeForm*(1.0/3.0)*(lastYearState.getITauxReu()/i.getMaxValue());
-                  
+
         resTauxReu += dLDotRecForm*(1.0/3.0)*(lastYearState.getITauxReu()/i.getMaxValue());
-                
+
         resTauxReu += dISatPers*(1.0/3.0)*(lastYearState.getITauxReu()/i.getMaxValue());
-                  
+
         i.setValue(resTauxReu);
-                                           
+
         //passage à l'année suivante
         int year = _year + 1;
         //calcul du budget de l'année suivante
