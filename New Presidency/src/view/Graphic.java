5786b751e2a960c0666a80d8f1380c572e559c24
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-11-16 21:36:01 +0100

    Implémentation des graphiques dans TextualView

--- New Presidency/src/view/Graphic.java
+++ New Presidency/src/view/Graphic.java
@@ -1,37 +1,188 @@
 package view;
 
-import org.knowm.xchart.QuickChart;
-import org.knowm.xchart.SwingWrapper;
+
+import model.State;
+import model.StateList;
 import org.knowm.xchart.XChartPanel;
 import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.HashMap;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+class Graphic{
 
-public class Graphic{
+    private JFrame _f;
+    private StateList _stateList;
+    private JComboBox<String> data1;
+    private JComboBox<String> data2;
+    private JComboBox<String> data3;
+    private JComboBox<String> data4;
+    private XYChart chart;
+    private HashMap<JComboBox<String>, String> _anciennesValeurs = new HashMap<>();
 
-    private JFrame f;
 
-    public Graphic(String name, String nameX, String nameY, String title, double[] dataX, double[] dataY){
+
+
+    Graphic(StateList stateList){
+        _stateList = stateList;
+        _f = new JFrame("Graphique d'évolution");
+
+
+        //Configuration du JFrame
+        _f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        _f.setLayout(new BorderLayout());
+        _f.setSize(1350, 1000);
+        _f.setResizable(false);
+
+        //Création de l'entête
+        JPanel entete = new JPanel();
+        _f.getContentPane().add(entete, BorderLayout.NORTH);
+
+        JLabel text = new JLabel("Choisissez une ou plusieurs données : ");
+        entete.add(text);
+
+        //Combobox
+        String[] valeurs = {"",
+                "Budget restant",
+                "Taux de réussite",
+                "Satisfaction du personnel",
+                "Satisfaction étudiante",
+                "Dotation récurante pour la formation",
+                "Dotation récurante pour la recherche",
+                "Dotation spécifique pour la formation",
+                "Prime de formation",
+                "Investissement en construction",
+                "Subventions aux associations étudiantes"};
+        data1 = new JComboBox<>(valeurs);
+        entete.add(data1);
+        data2 = new JComboBox<>(valeurs);
+        entete.add(data2);
+        data3 = new JComboBox<>(valeurs);
+        entete.add(data3);
+        data4 = new JComboBox<>(valeurs);
+        entete.add(data4);
+
+
+        _anciennesValeurs.put(data1, "");
+        _anciennesValeurs.put(data2, "");
+        _anciennesValeurs.put(data3, "");
+        _anciennesValeurs.put(data4, "");
+
+        //Si une des valeurs changes, on lance réaffiche le graphique
+        data1.addActionListener(actionEvent -> refresh(data1));
+        data2.addActionListener(actionEvent -> refresh(data2));
+        data3.addActionListener(actionEvent -> refresh(data3));
+        data4.addActionListener(actionEvent -> refresh(data4));
+
+
+
 
         //On crée le graphique
-        XYChart chart = QuickChart.getChart(name, nameX, nameY, title, dataX, dataY);
+        chart = new XYChartBuilder().width(800).height(600).title(getClass().getSimpleName()).xAxisTitle("Numéro de l'année").yAxisTitle("Valeur").build();
+        JPanel chartPanel = new XChartPanel<>(chart);
+        _f.getContentPane().add(chartPanel, BorderLayout.CENTER);
+
+        //On crée le bouton fermer
+        JPanel pied = new JPanel();
+        _f.getContentPane().add(pied, BorderLayout.SOUTH);
 
 
-        f = new JFrame(name);
-        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JButton button = new JButton("Fermer la fenêtre");
+        button.addActionListener(actionEvent -> close());
+        pied.add(button);
 
-        JPanel chartPanel = new XChartPanel(chart);
-        f.add(chartPanel);
-        f.pack();
-        f.setVisible(true);
+        _f.pack();
+        _f.setVisible(true);
     }
 
-    public void close(){
-        f.dispose();
+    private void refresh(JComboBox<String> data){
+        
+
+        //On met à jour le graphique
+        String datan = (String) data.getSelectedItem();
+
+        if(!datan.equals("")){
+            if(!_anciennesValeurs.containsValue(datan)){
+                chart.addSeries( datan, selectItems(datan));
+                chart.removeSeries(_anciennesValeurs.get(data));
+                _anciennesValeurs.replace(data, datan);
+
+            }
+
+
+        }
+        else{
+            if(!_anciennesValeurs.get(data).equals("")){
+                chart.removeSeries(_anciennesValeurs.get(data));
+            }
+        }
+        _f.repaint();
+
+
+
+
+
+
+    }
+
+    private double[] selectItems(String name){
+
+        double[] values = new double[_stateList.getStates().size()-1];
+
+        //Pour chaque état, on incrémente la liste des valeurs
+        int i=0;
+        for (State s:_stateList.getStates()) {
+            if (i != 0) {
+                switch (name){
+                    case "Budget restant":
+                        values[i-1] = s.getRemainingBudget();
+                        break;
+                    case "Taux de réussite":
+                        values[i-1] = s.getITauxReu();
+                        break;
+                    case "Satisfaction du personnel":
+                        values[i-1] = s.getISatPers();
+                        break;
+                    case "Satisfaction étudiante":
+                        values[i-1] = s.getISatEtu();
+                        break;
+                    case "Dotation récurante pour la formation":
+                        values[i-1] = s.getLDotRecForm();
+                        break;
+                    case "Dotation récurante pour la recherche":
+                        values[i-1] = s.getLDotRecRech();
+                        break;
+                    case "Dotation spécifique pour la formation":
+                        values[i-1] = s.getLDotSpeForm();
+                        break;
+                    case "Prime de formation":
+                        values[i-1] = s.getLPrime();
+                        break;
+                    case "Investissement en construction":
+                        values[i-1] = s.getLImmo();
+                        break;
+                    case "Subventions aux associations étudiantes":
+                        values[i-1] = s.getLSubAssoEtu();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+
+            i+=1;
+        }
+
+
+
+        return values;
+    }
+
+    void close(){
+        _f.dispose();
     }
 
 
