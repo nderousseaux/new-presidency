7c26109302c939c05bf878d8907d8ce548f4dee3
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-11-21 17:24:44 +0100

    Correction potentielle du problème des conteneurs des indicateurs et des leviers

--- New Presidency/src/Main.java
+++ New Presidency/src/Main.java
@@ -2,18 +2,12 @@ import controller.Controller;
 import view.TextualView;
 import view.GraphicalView;
 
-<<<<<<< HEAD
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, FileNotFoundException, IOException {
-=======
-import java.io.IOException;
-
-public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
->>>>>>> e4f312c2967af156800fe99092d80e90b9005013
+
         Controller controller=new Controller();
         controller.init();
         GraphicalView affichage = new GraphicalView(controller);
--- New Presidency/src/view/GraphicalView.java
+++ New Presidency/src/view/GraphicalView.java
@@ -13,6 +13,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
+import static java.lang.System.exit;
+
 
 public class GraphicalView extends JFrame {
     private Controller _controller;
@@ -28,7 +30,7 @@ public class GraphicalView extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(1200,850);
         this.setTitle("New Presidency");
-        this.setLayout(new BorderLayout());
+       // this.setLayout(new BorderLayout());
 
         majYear();
         majIndics();
@@ -45,23 +47,25 @@ public class GraphicalView extends JFrame {
         });
 
         nextTour.setText("Passer au tour suivant");
-
-        this.getContentPane().add(nextTour,BorderLayout.EAST);
-        this.getContentPane().add(_scrollIndicators);
-        this.getContentPane().add(_scrollLevers);
-        this.getContentPane().add(_budget,BorderLayout.SOUTH);
-        this.getContentPane().add(_year,BorderLayout.NORTH);
+        nextTour.setBounds(500,700,200,50);
+        this.getContentPane().add(nextTour);
+        this.add(_scrollIndicators,BorderLayout.EAST);
+        this.add(_scrollLevers);
+        this.add(_budget,BorderLayout.SOUTH);
+        this.add(_year,BorderLayout.NORTH);
+        this.setResizable(false);
         this.setVisible(true);
     }
 
     private void majBudget(){
         JLabel labBudget = new JLabel(String.valueOf(_controller.getBudget().getRemainingBudget()));
-
+        if(_budget!=null)
+            this.remove(_budget);
         _budget=new JPanel();
         _budget.add(new JLabel("Budget restant : "));
         _budget.add(labBudget);
-
-
+        this.add(_budget,BorderLayout.SOUTH);
+        this.setVisible(true);
     }
 
     private void majLevers(){
@@ -69,14 +73,13 @@ public class GraphicalView extends JFrame {
         _levers.setLayout(new GridLayout(_controller.getLevers().size(),1));
         _scrollLevers=new JScrollPane(_levers);
         _scrollLevers.setBounds(20,40,500,610);
-        _levers.setBounds(20,40,500,610);
 
         _levers.setToolTipText("Leviers");
         for(Lever l : _controller.getLevers()) {
             //element entier
             JPanel elem=new JPanel();
             elem.setLayout(new GridLayout(1,2));
-            elem.setSize(_scrollLevers.getSize());
+            //elem.setSize(_scrollLevers.getSize());
 
             //zone texte
 
@@ -138,22 +141,22 @@ public class GraphicalView extends JFrame {
 
             elem.add(nom);
             elem.add(zoneval);
-
+            elem.setToolTipText(((ArrayList<String>)l.getInfos()).get(0));
 
             _levers.add(elem);
 
         }
-
     }
 
     private void majIndics(){
         _indicators=new JPanel();
         _indicators.setLayout(new GridLayout(_controller.getIndicators().size(),1));
-        _indicators.setBounds(600,40,500,610);
+
         _scrollIndicators=new JScrollPane(_indicators);
         _scrollIndicators.setBounds(600,40,500,610);
+        _scrollIndicators.setPreferredSize(new Dimension(500,610));
 
-        _indicators.setToolTipText("Indicateurs");
+        //_indicators.setToolTipText("Indicateurs");
 
         for(Indicator i : _controller.getIndicators()){
             //element entier
@@ -173,6 +176,7 @@ public class GraphicalView extends JFrame {
             zoneval.add(val);
             elem.add(nom);
             elem.add(zoneval);
+            elem.setToolTipText(((ArrayList<String>)i.getInfos()).get(0));
             _indicators.add(elem);
         }
 
@@ -187,21 +191,25 @@ public class GraphicalView extends JFrame {
     }
 
     private void endOfRound(){
-        this.remove(_scrollLevers);
-        this.remove(_levers);
-        majLevers();
-        this.remove(_indicators);
-        this.remove(_scrollIndicators);
-        majIndics();
-        this.remove(_budget);
-        majBudget();
-        this.remove(_year);
-        majYear();
+        if(_controller.getYear()<=_controller.getMaxYear()) {
+            this.remove(_scrollLevers);
+            this.remove(_levers);
+            majLevers();
+            this.remove(_indicators);
+            this.remove(_scrollIndicators);
+            majIndics();
+            this.remove(_budget);
+            majBudget();
+            this.remove(_year);
+            majYear();
 
-        this.getContentPane().add(_scrollIndicators);
-        this.getContentPane().add(_scrollLevers);
-        this.getContentPane().add(_budget,BorderLayout.SOUTH);
-        this.getContentPane().add(_year,BorderLayout.NORTH);
-        this.setVisible(true);
+            this.getContentPane().add(_scrollIndicators,BorderLayout.EAST);
+            this.getContentPane().add(_scrollLevers);
+            this.getContentPane().add(_budget, BorderLayout.SOUTH);
+            this.getContentPane().add(_year, BorderLayout.NORTH);
+            this.setVisible(true);
+        }
+        else
+            exit(0);
     }
 }
