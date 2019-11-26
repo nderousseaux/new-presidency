877664a3ca4ad850e5f89e1113f64597c9277b40
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-11-26 14:54:54 +0100

    Documentation automatique !

---
+++ New Presidency/doc/html/search/all_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['textual_5finterface_15',['Textual_Interface',['../classview_1_1_textual___interface.html',1,'view']]],
+  ['textualview_16',['TextualView',['../classview_1_1_textual_view.html',1,'view']]]
+];
---
+++ New Presidency/doc/html/search/classes_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lever_25',['Lever',['../classmodel_1_1_lever.html',1,'model']]],
+  ['leverlist_26',['LeverList',['../classmodel_1_1_lever_list.html',1,'model']]]
+];
