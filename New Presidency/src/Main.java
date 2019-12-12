62ebfdb7fbdc0b32d4a5d13a9228f50d4d97bd5a
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-12-12 13:34:02 +0100

    Correction des erreurs de merge

--- New Presidency/src/Main.java
+++ New Presidency/src/Main.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         Controller controller=new Controller();
-        controller.init();
         GraphicalView affichage = new GraphicalView(controller);
     }
 }
